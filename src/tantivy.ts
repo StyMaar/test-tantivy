04683664b6f9c9df3b5459449f450d678d85c2b6
Guillaume Besson <guillaume@besson.co>
2022-03-31 17:08:02 +0200

    Add finalizers for rust classes to free up the memory

--- src/tantivy.ts
+++ src/tantivy.ts
@@ -3,10 +3,9 @@ import init, {
   Merger,
   SearchIndex as WasmSearchIndex,
   SegmentBuilder as WasmSegmentBuilder,
-  Segment,
+  Segment as WasmSegment,
   set_panic_hook
 } from "./pkg/tantivy_js";
-
 
 /**
  * Customize how tantivy is loaded
@@ -44,11 +43,15 @@ export type IndexSchema<Fields extends string> = {
   }
 }
 
+// Free up the Rust memory when the SegmentBuilder class is not used anymore
+const segmentBuildersFinalizationRegistry = new FinalizationRegistry<WasmSegmentBuilder>((wasmSegmentBuilder) => wasmSegmentBuilder.free());
+
 export class SegmentBuilder<Fields extends string> {
   private wasmSegmentBuilder: WasmSegmentBuilder;
 
   constructor(schema: IndexSchema<Fields>, memoryArenaNumBytes = 50_000_000) {
     this.wasmSegmentBuilder = new WasmSegmentBuilder(schema, memoryArenaNumBytes);
+    segmentBuildersFinalizationRegistry.register(this, this.wasmSegmentBuilder);
   }
 
   addDocument(document: {[field in Fields]: string}) {
@@ -60,23 +63,27 @@ export class SegmentBuilder<Fields extends string> {
   }
 
   finalize() {
-    return this.wasmSegmentBuilder.finalize();
+    segmentBuildersFinalizationRegistry.unregister(this);
+    return new Segment(this.wasmSegmentBuilder.finalize());
   }
 }
 
+// Free up the Rust memory when the SearchIndex class is not used anymore
+const searchIndexFinalizationRegistry = new FinalizationRegistry<WasmSearchIndex>((wasmSearchIndex) => wasmSearchIndex.free());
 export class SearchIndex<Fields extends string> {
   private wasmSearchIndex: WasmSearchIndex;
 
   constructor() {
     this.wasmSearchIndex = new WasmSearchIndex();
+    searchIndexFinalizationRegistry.register(this, this.wasmSearchIndex);
   }
 
   registerSegment(segment: Segment) {
-    this.wasmSearchIndex.registerSegment(segment);
+    this.wasmSearchIndex.registerSegment(segment._getWasmSegment());
   }
 
   removeSegment(segment: Segment) {
-    this.wasmSearchIndex.removeSegment(segment);
+    this.wasmSearchIndex.removeSegment(segment._getWasmSegment());
   }
 
   search(query: string, options: {limit?: number, fields?: (Fields)[] } = {}): {[field in Fields]: string}[] {
@@ -88,4 +95,35 @@ export class SearchIndex<Fields extends string> {
   }
 }
 
-export { Segment, Merger };
+// Free up the Rust memory when the Segment class is not used anymore
+const segmentFinalizationRegistry = new FinalizationRegistry<WasmSegment>((wasmSegment) => wasmSegment.free());
+export class Segment {
+  private wasmSegment: WasmSegment;
+
+  /**
+   * Create a new Segment from raw data of a previously exported segment
+   * @param data raw data from Segment.export, only as Uint8Array
+   */
+  constructor(data: Uint8Array | WasmSegment) {
+    if (data instanceof Uint8Array) {
+      this.wasmSegment = new WasmSegment(data);
+    } else {
+      this.wasmSegment = data;
+    }
+    segmentFinalizationRegistry.register(this, this.wasmSegment);
+  }
+
+  /**
+   * Export the raw data in a segment as a Uint8Array
+   * @returns raw data as Uint8Array
+   */
+  export() {
+    return this.wasmSegment.export();
+  }
+
+  _getWasmSegment() {
+    return this.wasmSegment;
+  }
+}
+
+export { Merger };
